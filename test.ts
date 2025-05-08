6d86de343956237afb38064cb79ce0a75202ca27
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2025-05-08 10:50:39 -0300

    modificado arquivo tsconfig

--- test.ts
+++
@@ -1,5 +0,0 @@
-const nome = 'Edmar' // ← aspas duplas (vai dar erro se você exige aspas simples)
-
-function ola() {
-  const x = 10 // ← sem ponto e vírgula (vai dar erro se você exige ponto e vírgula)
-}
